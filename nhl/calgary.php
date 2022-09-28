b0f51ac2879b8bf9033ef1e340a158b7bc03fad2
Liamcr <92156381+Liamcr21@users.noreply.github.com>
2022-09-28 15:51:30 +0200

    lcr

--- nhl/calgary.php
+++ nhl/calgary.php
@@ -20,7 +20,7 @@ use YR72dpi\BDD;
   <header>
     <nav class="navbar" role="navigation">
         <div class="nav-logo">
-            <a href="../index.html"><img class="logo" src="/img/logo_white.png" alt="Logo LCRBET"></a>
+            <a href="../index.html"><img class="logo" src="/img/logo-nav.png" alt="Logo LCRBET"></a>
         </div>           
         <ul class="navbar-links">
             <li class="navbar-link first lien nav-active"><a href="../ps.html" >Paris sportif</a></li>
@@ -38,7 +38,6 @@ use YR72dpi\BDD;
 
     <h1 class="h1-algo"> Prédiction<br> Calgary Flames</h1>
     <img src="img-nhl/calgary.png" alt="" class="img-ny">
-    <p class="txt-hero-algo">blablacla</p>
 
 
     <div class="admin-algo">
@@ -47,12 +46,12 @@ use YR72dpi\BDD;
         <h2 class="name-match">Calgary - Colorado <br> timer</h2>
         <img src="img-nhl/calgary.png" alt="" class="img-ny">
     </div>
+    <h2 class="titre-general">Prédiction Général</h2>
     <div class="res-general">
-        <h2 class="titre-general">Prédiction Général</h2>
         <div class="res-general2">
-        <h2 class="titre-predi">Nombre de but</h2>
-        <h2 class="titre-predi">Vainqueur</h2>
-        <h2 class="titre-predi">Prolongation</h2>
+        <h2 class="titre-predi titre-predi-top">Nombre de but</h2>
+        <h2 class="titre-predi titre-predi-top">Vainqueur</h2>
+        <h2 class="titre-predi titre-predi-top">Prolongation</h2>
     </div>
 
     <?php
@@ -80,18 +79,23 @@ use YR72dpi\BDD;
         <h2 class="titre-predi">Nombre de but Colorado</h2>
     </div>
     <div class="res-general2">
-        <div class="res-on-modif">
+        <div class="res-on-modif-">
             <p class="predi-txt"><?= $req->fetch(PDO::FETCH_ASSOC)["Moyenne_but_Calgary"] ?></p>
         </div>
-        <div class="res-on-modif">
+        <div class="res-on-modif-2">
             <p class="predi-txt">4,6</p>
         </div>
     </div>
 
     </div>
+    <div class="titre-gen">
+    <h2 class="titre-general">1ère mi-temps</h2>
+    <h2 class="titre-general">2ème mi-temps</h2>
+    <h2 class="titre-general">3ème mi-temps</h2>
+</div>
+
     <div class="res-1mt">
         <div class="col-1mt1">
-            <h2 class="titre-general">1ère mi-temps</h2>
             <h2 class="titre-predi">Nombre de but</h2>
             <p class="predi-mt">4,6</p>
             <h2 class="titre-predi">Nombre de but Calgary</h2>
@@ -102,7 +106,6 @@ use YR72dpi\BDD;
             <p class="predi-mt">4,6</p>
         </div>
         <div class="col-1mt2">
-            <h2 class="titre-general">2ème mi-temps</h2>
             <h2 class="titre-predi">Nombre de but</h2>
             <p class="predi-mt">4,6</p>
             <h2 class="titre-predi">Nombre de but Calgary</h2>
@@ -113,7 +116,6 @@ use YR72dpi\BDD;
             <p class="predi-mt">4,6</p>
         </div>
         <div class="col-1mt3">
-            <h2 class="titre-general">3ème mi-temps</h2>
             <h2 class="titre-predi">Nombre de but</h2>
             <p class="predi-mt">4,6</p>
             <h2 class="titre-predi">Nombre de but Calgary</h2>
@@ -132,8 +134,8 @@ use YR72dpi\BDD;
 
     <footer class="footer-distributed">
 			<div class="footer-right">
-				<a href="https://discord.gg/7zHXJ4wX"><img src="img/discord.png" class="footer-img"></a>
-        <a href="https://twitter.com/lcrbet"> <img src="img/twitter.png" class="footer-img"></a>
+				<a href="https://discord.gg/7zHXJ4wX"><img src="/img/discord.png" class="footer-img"></a>
+        <a href="https://twitter.com/lcrbet"> <img src="/img/twitter.png" class="footer-img"></a>
 			</div>
 			<div class="footer-left">
 				<p>LCRBET &copy; 2022 - Par <a href="https://liamcariou.fr/" style="text-decoration:none;color: white;">Liamcariou.fr</a></p>
