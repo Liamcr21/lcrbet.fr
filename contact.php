589784154fdf8dc35d8188006e5790f1b2954887
Liamcr <92156381+Liamcr21@users.noreply.github.com>
2022-09-28 16:34:47 +0200

    lcr

--- contact.php
+++ contact.php
@@ -1,3 +1,42 @@
+<?php
+if($_POST):
+$prenom=$_POST["firstname"];
+$nom=$_POST["lastname"];
+$mail=$_POST["email"];
+$msg=$_POST["message"];
+$sujet=$_POST["sujet"];
+
+$texte= "Nouveau message de ".$prenom." ".$nom." ".$sujet." (".$mail.") : ".$msg;
+
+$headers = 'From:'.$mail . "\r\n" . 
+          'Reply-To:'.$mail . "\r\n" .
+         'X-Mailer: PHP/' . phpversion();
+
+         if(mail("cariou.liam@orange.fr", "Nouveau message", $texte,$headers )):
+
+             $sqlQuery = 'INSERT INTO liamcariou(nom, prenom, sujet, msg, email) VALUES ("'.$nom.'", "'.$prenom.'", "'.$sujet.'", "'.$msg.'", "'.$mail.'")';
+
+            $mysqlConnection = new PDO(  'mysql:host=localhost;dbname=u801021231_liamcariou;charset=utf8',
+              'u801021231_liamcariou',
+                'ftp21pir@teFTP21'
+              );
+
+
+            $mysqlConnection->query($sqlQuery); 
+
+            //var_dump($mysqlConnection->errorInfo());
+
+            $confirme="oui";
+            endif;
+
+            endif;
+            ?>
+
+            <?php
+
+            ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +46,7 @@
     <title>Accueil - LCRBET - LCRBET.fr</title>
     <link rel="stylesheet" href="/css/navbar-sf.css">
     <link rel="stylesheet" href="/css/maintenance.css">
+    <link rel="stylesheet" href="/css/contact.css">
     <link rel="stylesheet" href="/css/footer.css">
     <link rel="icon" href="img/logo_white.png"></title>
 </head>
@@ -28,11 +68,40 @@
     </nav>        
 </header>
 <body>
-<section class="maintenance">
-        <div class="loader"></div>
-        <h1 class="h1-maint">Page en construction...</h1>
-        <img src="img/logo_white.png" alt="logo" class="img-maint">
-    </section>
+
+
+<h1 class="h1-contact">Contact LCRBET</h1>
+<div class="container">
+
+  <?php
+if ($confirme=="oui"):
+  ?>
+<p>Votre message a bien été envoyé ! Je vous y reponds au plus vite. </p>
+ <?php
+ else:
+ ?>
+  <form action="/action_page.php">
+    <label for="fname">Prénom</label>
+    <input type="text" id="fname" name="firstname" placeholder="Votre prénom...">
+
+    <label for="lname">Nom</label>
+    <input type="text" id="lname" name="lastname" placeholder="Votre nom ...">
+
+    <label for="lname">email</label>
+    <input type="text" id="lname" name="email" placeholder="Votre email..">
+
+    <label for="subject" name="sujet">Sujet</label>
+    <textarea id="message" name="message" placeholder="Votre message ..." style="height:200px"></textarea>
+
+    <input type="submit" value="Submit">
+  </form>
+  <?php
+  endif;
+  ?>
+</div>
+
+</body>
+</html>
      
     <footer class="footer-distributed">
 			<div class="footer-right">
