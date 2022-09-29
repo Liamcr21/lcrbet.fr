da430c35efbec3938ed59173739e8cb7d5968545
Rousselle <ylan.rousselle76@gmail.com>
2022-09-29 13:59:13 +0200

    revert

--- BDD.php
+++ BDD.php
@@ -7,16 +7,7 @@ namespace lcrbet;
 class BDD {
     
     public static function connect(string $cd = null) {
-=======
-<<<<<<< HEAD
         $dsn = 'mysql:dbname=lcrbet;host=localhost'; $user = 'root'; $password = '';
-=======
-        $dsn = 'mysql:dbname=u801021231_BDDNHLlcrbet;host=localhost'; $user = 'u801021231_lcrbetadminbdd'; $password = 'mdp';
->>>>>>> 68ef818fdf03dc13c6b0213015a3aebf69b8aed4
->>>>>>> parent of 27e0b6b (lcr)
-
-
-        
 
          try {
              $dbh = new \PDO($dsn, $user, $password);
