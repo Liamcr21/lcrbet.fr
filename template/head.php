8ee647579c47a6f2823419c5906d4e5660105e3c
Rousselle <ylan.rousselle76@gmail.com>
2022-10-05 16:08:51 +0200

    add template

---
+++ template/head.php
@@ -0,0 +1,9 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact - LCRBET - LCRBET.fr</title>
+    <link rel="stylesheet" href="./sources/style.css">
+    <link rel="stylesheet" href="./sources/navbar-sansfond.css">
+    <link rel="icon" href="img/logo_white.png"></title>
+</head>
