6798076c237b09ece27d96408ac0bdb3e5e1d5a7
Liam Cariou <cariou.liam@orange.fr>
2022-10-07 15:57:58 +0200

    algo

--- algorithme-de-prediction.php
+++ algorithme-de-prediction.php
@@ -1,7 +1,20 @@
 <?php ob_start(); ?>
 
+<h1 class="titre-algo-principal"> Algorithme de prédiction</h1>
+    <p class="sous-titre-algo">Choississez votre championnat</p>
+
+    <div class="select-champ">
+        <img src="img/logo-nhl.png" class="img-algo" alt="" >
+        <img src="img/ligue1.png" class="img/algo" alt="">
+        <img src="img/premierleague.png" class="img-alo" alt="">
+        <img src="img/bundes.png" class="img-algo" alt="">
+        <img src="img/liga.png" class="img-algo" alt="">
+    </div>
 
 
+    <h2 class="sous-titre-algo">En savoir plus sur l'algorithme</h2>
+    <p class="txt-algo">L’algorithme se base sur les derniers résultats des deux équipes qui s’affronte et prend aussi en compte les derniers matchs pour analyser la forme de ces équipes<br>
+     L’algorithme est 100% gratuit</p>
 
 
 
